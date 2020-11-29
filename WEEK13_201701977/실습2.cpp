8b433f8c022d7a9ac03a4cc32c1a4ddf36323083
una.kwon <yunaghgh@naver.com>
2020-11-29 19:44:52 +0900

    Update 실습2.cpp

--- WEEK13_201701977/실습2.cpp
+++ WEEK13_201701977/실습2.cpp
@@ -1,4 +1,7 @@
 /* Template-variadic template */
+// 마지막 sum 호출 시 파라미터가 하나만 남는다.
+// sum 함수는 파라미터를 두개 받기때문에 호출될 수 없으므로 컴파일 에러가 발생한다.
+// 그래서 파라미터를 하나 가지는 sum함수를 template으로 생성하고 정의한다.
 
 #include <iostream>
 #include <string>
